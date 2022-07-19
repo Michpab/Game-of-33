a938a2d9ce065cbfa73876f1b780e44375200a19
Michpab <michpab11@yahoo.com>
2022-07-19 16:42:43 -0400

    updated HTML

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 var playerCount = 0;
 var dealerCount = 0;
 
+function draw()
+var values = ["1","2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var deck = new Array();
+var players = new Array();
+var currentPlayer = 0;
 
-function draw(){
-    return parseInt(Math.random() * 10 + 1)
+function createDeck(){
+deck = new Array();
+for (var i = 0 ; i < values.length; i++)
+deck.push(card);
 }
-
 
 function playerStay(){
-    alert("Player Stays");
+alert("Player Stays");
 }
-
 function playerHit(){
-    playerCount=playerCount + 1;
-    let card = draw();
-    document.getElementById("P" + playerCount).innerHTML = card;
-}
-    
-
-
-
+playerCount=playerCount + 1;
+let card = draw();
+document.getElementById("P" + playerCount).innerHTML = card;}
 function dealerStay(){
-    alert("Dealer Stay");
+alert("Dealer Stay");
 }
-
 function dealerHit(){
-    dealerCount=dealerCount + 1;
-    let card = draw();
-    document.getElementById("D" + dealerCount).innerHTML = card;
+dealerCount=dealerCount + 1;
+let card = draw();
+document.getElementById("D" + dealerCount).innerHTML = card;
 }  
     
  
